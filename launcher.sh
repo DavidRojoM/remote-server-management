f30620571c8278a38d05c5cb58c16b78016f918c
David Rojo <davidrojo1997@gmail.com>
2021-12-27 15:37:15 +0100

    feat(launcher): add select menu

--- launcher.sh
+++ launcher.sh
@@ -1,1 +1,19 @@
 #!/bin/bash
+
+PS3='Please enter your choice: '
+options=("Administrar IPs" "Administrar servidores" "Salir")
+select opt in "${options[@]}"
+do
+    case $opt in
+        "Administrar IPs")
+            sh check-disk-free.sh
+            ;;
+        "Administrar servidores")
+            echo "you chose choice 2"
+            ;;
+        "Salir")
+            break
+            ;;
+        *) echo "invalid option $REPLY";;
+    esac
+done
