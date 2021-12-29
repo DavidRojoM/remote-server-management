fab567003b0289c6aa7783f5be0848c0f5f9ca50
David Rojo <davidrojo1997@gmail.com>
2021-12-29 22:53:33 +0100

    feat(list-ips): add ip list check, error messages and console clearing

--- list-ips.sh
+++ list-ips.sh
@@ -1,8 +1,10 @@
 #!/bin/bash
 
-clear
+if [[ -e ip-list.txt ]]; then
 cat -n ip-list.txt
+else echo 'No hay ninguna ip'
+fi
 
 read -rp "Pulse una tecla para continuar : " continue
-
+clear;
 sh ip-administration.sh
