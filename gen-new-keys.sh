57601797bbb2e0fc1e07dca3b642f796dba1cb1f
David Rojo <davidrojo1997@gmail.com>
2021-12-30 23:58:20 +0100

    feat(gen-new-keys): add key generation implementation

--- gen-new-keys.sh
+++ gen-new-keys.sh
@@ -1,1 +1,20 @@
 #!/bin/bash
+
+
+if ! [[ -e ip-list.txt ]] || [ "$(cat ip-list.txt | wc -l)" -gt 0 ]; then
+  clear
+  echo 'No has insertado ninguna ip'
+  sleep 1
+  clear
+  sh server-administration.sh
+fi
+
+if [[ -e server-administration-key ]] && [[ -e server-administration-key.pub ]]; then
+  rm -f server-administration-key server-administration-key.pub
+fi
+ssh-keygen -C server-administration-key -N '' -f server-administration-key <<< y 1>/dev/null 2>>logs/error.log
+clear
+echo -e "La nueva key es \n\n$(cat server-administration-key)\n"
+read -rp "Presione enter para continuar" continue
+clear
+sh server-administration.sh
