2de3961e9de0a4f9f28aa79733b4dfdca707e1e1
David Rojo <davidrojo1997@gmail.com>
2022-01-02 20:29:28 +0100

    feat(server-status): add impl

--- server-status.sh
+++ server-status.sh
@@ -1,1 +1,21 @@
 #!/bin/bash
+
+source ./colors.sh
+clear
+if ! [ -e ip-list.txt ] || [ "$(cat ip-list.txt | wc -l)" -eq 0 ]; then
+    echo -e "\n${YELLOW}Debes insertar alguna ip${ENDCOLOR}\n"
+    read -rp "Presione enter para continuar..."
+    sh server-administration.sh
+fi
+
+source ./check-server-status.sh
+while read -r line; do
+    ip=$(echo "$line" | cut -f2)
+  if is_server_online "$ip"; then
+      echo -e "${GREEN}$ip\t->\tOnline${ENDCOLOR}"
+      else echo -e "${RED}$ip\t->\tOffline${ENDCOLOR}"
+  fi
+done < ip-list.txt
+echo
+read -rp "Presione enter para continuar..."
+sh server-administration.sh
