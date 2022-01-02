9ced358f417fc69213e707310282de43ccf3c9e8
David Rojo <davidrojo1997@gmail.com>
2022-01-02 20:29:05 +0100

    feat(list-ips): add message colors

--- list-ips.sh
+++ list-ips.sh
@@ -1,10 +1,12 @@
 #!/bin/bash
 
+source ./colors.sh
+
 if [[ -e ip-list.txt ]]; then
 cat -n ip-list.txt
-else echo 'No hay ninguna ip'
+else echo -e "${YELLOW}No hay ninguna ip${ENDCOLOR}"
 fi
 
-read -rp "Pulse una tecla para continuar : " continue
+read -rp "Pulse enter para continuar... "
 clear;
 sh ip-administration.sh
