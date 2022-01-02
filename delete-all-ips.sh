096f2026fc7803f25c9990ff7708ac6a47373bd8
David Rojo <davidrojo1997@gmail.com>
2022-01-02 20:27:53 +0100

    feat(delete-all-ips): add feedback messages and shell colors

--- delete-all-ips.sh
+++ delete-all-ips.sh
@@ -1,4 +1,14 @@
 #!/bin/bash
 
+source ./colors.sh
+
 cat /dev/null > ip-list.txt
+
+list_length=$(cat ip-list.txt | wc -l)
+if [ "$list_length" -eq 0 ]; then
+    echo -e "\n${GREEN}Eliminadas correctamente${ENDCOLOR}\n"
+    else echo -e "\n${RED}No se ha podido eliminar${ENDCOLOR}\n"
+fi
+read -rp "Pulse enter para continuar... "
+clear
 sh ip-administration.sh
