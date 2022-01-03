65859e5246e73301ac5c6d6ab8151a8670c27a67
David Rojo <davidrojo1997@gmail.com>
2022-01-03 23:22:51 +0100

    fix(check-disk-free): fix message header

--- check-disk-free.sh
+++ check-disk-free.sh
@@ -10,12 +10,12 @@ if ! [ -e ip-list.txt ] || [ "$(cat ip-list.txt | wc -l)" -eq 0 ]; then
 fi
 
 source ./check-server-status.sh
+      echo -e "IP\t\t\tDisco"
 while read -r line; do
       user=$(echo "$line" | cut -f1)
       ip=$(echo "$line" | cut -f2)
   if is_server_online "$ip"; then
       storage=$(ssh -o StrictHostKeyChecking=no -i server-administration-key -n "$user"@"$ip" "df -H | grep '% /$'")
-      echo -e "IP\t\t\tDisco"
       echo -e "${GREEN}$ip${ENDCOLOR}\t->\t${YELLOW}$storage${ENDCOLOR}"
       else echo -e "${GREEN}$ip${ENDCOLOR}\t->\t${RED}Offline${ENDCOLOR}"
 
